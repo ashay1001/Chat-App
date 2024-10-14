d8343985ee7838d6f8a64a4befd01274d65058b1
as1001 <ashay165@gmail.com>
2024-10-14 20:36:56 +0530

    Completed Login and Signup

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -6,9 +6,11 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useState } from "react"
 import { toast } from "sonner";
 import apiClient from "@/lib/api-client";
-import { SIGNUP_ROUTE } from "@/utils/constants";
+import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants";
+import { useNavigate } from "react-router-dom";
 function Auth() {
 
+  const navigate = useNavigate()
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -29,12 +31,40 @@ function Auth() {
     return true;
   }
 
-  const handleLogin = async () => {};
+  const validateLogin = () => {
+    if(!email.length) {
+      toast.error("Email is required.");
+      return false;
+    }
+    if(!password.length) {
+      toast.error("Password is required.");
+      return false;
+    }
+    return true;
+  }
+
+  const handleLogin = async () => {
+    if(validateLogin) {
+      const response = await apiClient.post(LOGIN_ROUTE, {email, password}, {withCredentials: true});
+      if(response.data.user.id) {
+        if(response.data.user.profileSetup) {
+          navigate("/chat");
+        }
+        else {
+          navigate("/profile");
+        }
+      }
+      console.log(response);
+    }
+  };
   
   const handleSignup = async () => {
     if(ValidateSignup()) {
-      const response = await apiClient.post(SIGNUP_ROUTE, {email, password});
+      const response = await apiClient.post(SIGNUP_ROUTE, {email, password}, {withCredentials: true});
       console.log(response);
+      if(response.status === 201) {
+        navigate("/profile");
+      }
     }
   };
 
@@ -50,7 +80,7 @@ function Auth() {
             <p className="font-medium text-center"> Fill the details to get started!</p>
           </div>
           <div className="flex items-center justify-center w-full">
-            <Tabs className="w-3/4">
+            <Tabs className="w-3/4" defaultValue="login">
               <TabsList className="w-full">
                 <TabsTrigger value="login" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:font-semibold data-[state=active]:text-black data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Login</TabsTrigger>
                 <TabsTrigger value="signup" className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:font-semibold data-[state=active]:text-black data-[state=active]:border-b-purple-500 p-3 transition-all duration-300">Signup</TabsTrigger>
--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,3 +1,4 @@
+import { compare } from "bcrypt";
 import User from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 
@@ -24,6 +25,46 @@ export const signup = async (request, response, next) => {
         id: user.id,
         email: user.email,
         profileSetup: user.profileSetup,
+      },
+    });
+  }
+  catch (error) {
+    console.log(error);
+    return response.status(500).send("Internal Server Error");
+  }
+} 
+
+export const login = async (request, response, next) => {
+  try {
+    const { email, password } = request.body;
+    if (!email || !password) {
+      return response.status(400).send("Email and Password Required")
+    }
+    const user = await User.findOne({email});
+    if(!user) {
+      return response.status(404).send("User not found.");
+    }
+
+    const auth = await compare(password, user.password);
+    if(!auth) {
+      return response.status(400).send("Pasword is incorrect.");
+    }
+
+    response.cookie("jwt", createToken(email, user.id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return response.status(200).json({
+      user: {
+        id: user.id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        image: user.image,
+        color: user.color,
       },
     });
   }
